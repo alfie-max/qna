0774d571d80fbfbde881bdea8dc5ae82fdbf6d95
Alfred Dominic <mailme@alfiemax.com>
2020-03-14 11:49:49 +0530

    add question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,25 @@ class QuestionsController < ApplicationController
   def index
 
   end
+
+  def new
+    @question = Question.new
+    @question.build_topic
+  end
+
+  def create
+    @question = current_user.questions.new(question_params)
+
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, topic_attributes: [:name])
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :topic
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
+
+  accepts_nested_attributes_for :topic,
+                                reject_if: ->(s) { s[:name].blank? },
+                                allow_destroy: true
 end
